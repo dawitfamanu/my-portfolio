f60ddd44dce3ebf8a02fb4abb3ca60d99b9fa05c
Dawit Fikru Amanu <58340792+dawitfamanu@users.noreply.github.com>
2025-04-19 18:02:07 +0900

    update next config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  basePath: "/my-portfolio",
+  output: "export", // enables static exports
+  reactStrictMode: true,
   /* config options here */
 };
 
