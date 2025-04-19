5bd7722cd6850e20a40634a07708e93fe6efa9c7
Dawit Fikru Amanu <58340792+dawitfamanu@users.noreply.github.com>
2025-04-20 08:59:44 +0900

    add assetprefix to next config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   basePath: "/my-portfolio",
+  assetPrefix: "/my-portfolio/",
   output: "export", // enables static exports
   reactStrictMode: true,
   /* config options here */
