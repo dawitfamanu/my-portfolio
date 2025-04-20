fa4930f208a6fce42a18707ea8f51d3f70208068
Dawit Fikru Amanu <58340792+dawitfamanu@users.noreply.github.com>
2025-04-20 09:29:01 +0900

    update assetPrefix in next.config and modify resume link in page component

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   basePath: "/my-portfolio",
-  assetPrefix: "/my-portfolio/",
   output: "export", // enables static exports
   reactStrictMode: true,
   /* config options here */
